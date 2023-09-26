1b365ed4e1e526b4ab8ab7bd6e8211382a95050b
MrPlax <aadam.blank@gmail.com>
2023-09-26 09:46:52 +0200

    ggg

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -3,6 +3,53 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//napisz program który wczyta liczbê od u¿ytkownika i j¹ wyœiwtli na konsoli	
+	std::cout << "Podaj liczbe/n";
+	int numberFromUser;
+	std::cin >> numberFromUser;
+	std::cout << "Podales " <<  numberFromUser << "/n";
 }
 //algorytm- skoñczony zbiór instrukcji/zadañ który rozwi¹zuje zadany problem
+/*   
+Zapis algorytmów
+-rysunki
+-opis s³owny
+-w punktach
+-schemat blokowy
+-kod Ÿród³owy danego jêzyka programowania
+-pseudokod
+
+Zmienna- pewien obszar w pamiêci operaacyjnej, w której mo¿na w danej chwiliprzechowaæ tylko jedn¹ dan¹.
+
+Instrukcja deklaracji zmiennej:
+typ_zmiennej nazwa_zmiennej;
+
+Typ zmiennej-wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów
+int- l. ca³kowita ze znakiem < -2 147 483 648, 2 147 483 648 >
+short- 2 bajtowa l. ca³kowita ze znakiem <-32 768, 32 767>
+long- to samo co int
+long long- 8 bajtowa l. ze znakiem <-9 223 372 036 854 775 808, 9 223 372 036 854 775 807>
+unsigned- zmienna bez znaku  <0, 2*max + 1>
+float- 4 bajtowa l. rzeczywista, dok³adnoœæ 6-7 cyfr po przecinku 
+long double- 12 bajtowa l.rzeczywista, dok³adnoœæ 19 - 20 cyfr po przecinku
+double- 8 bajtowa l.rzeczywista, dok³adnoœæ 15 - 16 cyfr po przecinku 
+
+Nazwa zmienne nazwa obszaru w pamiêci
+Warunki niezbdne
+-alfabet angielski aA do zZê
+-Cyfry arabskie 0-9
+-podkreœlenie (pod³oga)
+-pierwszym znakiem nie mo¿e byc cyfra
+-unikalny  w swoim zakresie widocznoœci
+-nie mo¿e byæ to s³owo kluczowe (zarezerwowane) danego jêzyka
+
+Warunki programistów
+- nazwa zmiennej powinna oddawæ charakter przechowywanych danych
+- jeœli wiele s³ów to w miejsce spacji podkreœlenie lub zaczynaja od drugiego s³owa piszemy je z du¿ej litery
+-piszemy po angielsku
+
+
+
+
+*/
+
