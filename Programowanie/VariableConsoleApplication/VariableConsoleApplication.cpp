866a498adcdd1e726f797351fda03a7148f72dce
MrPlax <aadam.blank@gmail.com>
2023-10-03 09:53:16 +0200

    koniec

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -1,16 +1,90 @@
 
 #include <iostream>
 
+
+
+//napisz program który wczyta liczbê od u¿ytkownika i j¹ wyœiwtli na konsoli	
+void task1()
+{
+	int numberFromUser;
+	std::cout << "Podaj liczbe\n";
+	std::cin >> numberFromUser;
+	std::cout << "Podales " << numberFromUser << "\n";
+
+}
+
+//Program obliczaj¹cy œredni¹ arytmetyczn¹ dwóvh liczb
+void task2()
+{
+	int firstNumber, secondNumber;
+	std::cout << "Podaj pierwsza liczbe\n";
+	std::cin >> firstNumber;
+	std::cout << "Podaj druga liczbe\n";
+	std::cin >> secondNumber;
+	int average;
+	average = (firstNumber + secondNumber) / 2;
+
+	std::cout << "Srednia to: " << average << "\n";
+
+}
+//Program do obliczania pola prostok¹ta
+void task3()
+{
+	int sideA, sideB;
+	std::cout << "Podaj d³ugoœæ pierwszego boku \n ";
+	std::cin >> sideA;
+	std::cout << "Podaj d³ugoœæ pierwszego bko \n ";
+	std::cin >> sideB;
+	int area;
+	area = sideA * sideB;
+	std::cout << "Pole wynosi : " << area << "\n ";
+}
+//Program obliczaj¹cy objêtoœæ sto¿ka
+void task4()
+{
+	int ray, height;
+	std::cout << "Podaj d³ugoœæ promienia \n ";
+	std::cin >> ray;
+	std::cout << "Podaj d³ugoœæ wysokoœci";
+	std::cin >> height;
+	int volume;
+	volume = (height / 3) * 3.1415 * (ray * ray);
+	std::cout << "osto¿ka wynosi : " << volume << "\n";
+}
+
+//Program obliczaj¹cy pole ko³a.
+void task5()
+{
+	int ray;
+		std::cout << "Podaj d³ugoœæ promienia";
+		std::cin >> ray;
+		int area;
+		area = 3.1415 * (ray * ray);
+		std::cout << "Pole d³ugoœci wynosi : " << area << "\n";
+
+
+
+}
+
+//Program obliczaj¹cy wartoœæ wyra¿enia a^2 + b^2
+
+
+
+
 int main()
 {
-	//napisz program który wczyta liczbê od u¿ytkownika i j¹ wyœiwtli na konsoli	
-	std::cout << "Podaj liczbe/n";
-	int numberFromUser;
-	std::cin >> numberFromUser;
-	std::cout << "Podales " <<  numberFromUser << "/n";
+	setlocale(LC_CTYPE, "polish");
+     //task1()
+	//task2()
+	//task3()
+	//task4()
+	//task5()
+	task6()
 }
+
+
 //algorytm- skoñczony zbiór instrukcji/zadañ który rozwi¹zuje zadany problem
-/*   
+/*
 Zapis algorytmów
 -rysunki
 -opis s³owny
@@ -30,9 +104,9 @@ short- 2 bajtowa l. ca³kowita ze znakiem <-32 768, 32 767>
 long- to samo co int
 long long- 8 bajtowa l. ze znakiem <-9 223 372 036 854 775 808, 9 223 372 036 854 775 807>
 unsigned- zmienna bez znaku  <0, 2*max + 1>
-float- 4 bajtowa l. rzeczywista, dok³adnoœæ 6-7 cyfr po przecinku 
+float- 4 bajtowa l. rzeczywista, dok³adnoœæ 6-7 cyfr po przecinku
 long double- 12 bajtowa l.rzeczywista, dok³adnoœæ 19 - 20 cyfr po przecinku
-double- 8 bajtowa l.rzeczywista, dok³adnoœæ 15 - 16 cyfr po przecinku 
+double- 8 bajtowa l.rzeczywista, dok³adnoœæ 15 - 16 cyfr po przecinku
 
 Nazwa zmienne nazwa obszaru w pamiêci
 Warunki niezbdne
