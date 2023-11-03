84748a5835402b9d714c11d6e09f6c60679856c8
MrPlax <aadam.blank@gmail.com>
2023-11-03 14:22:35 +0100

    hdh

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -34,8 +34,49 @@ void task1()
 	std::cout << "Liczba dodatnia pobrana od uzytkownika " << numberFromUser << "\n";
 }
 
+
+//Napisz program, który ma pobraæ od u¿ytkownika liczbê dodatni¹. 
+//Zabezpiecz program przed pobieraniem liczb ujemnych.
+void task2()
+{
+	int numberFromUser;
+
+	do
+	{
+		std::cout << "Podaj liczbê doodatni¹:\n";
+		std::cin >> numberFromUser;
+	} while (numberFromUser < 0);
+
+	std::cout << "Liczba dodatnia pobrana od uzytkownika " << numberFromUser << "\n";
+}
+
+//Napisz program, który wylosuje liczbe, a u¿ytkownik bêdzie musia³ j¹ zgadn¹æ.
+
+void task3()
+{
+	int randomNumber, numberFromUser;
+	do
+	{
+		randomNumber = rand();
+		std::cout << "Podaj liczbe \n";
+		std::cin >> numberFromUser;
+		std::cout << randomNumber; 
+	} while (numberFromUser = randomNumber);
+		std::cout << "WYGRA£EŒ\n";
+
+	
+
+
+
+}
+
+
+
+
+
 int main()
 {
-
-
+	//task1();
+	//task2();
+	task3();
 }
