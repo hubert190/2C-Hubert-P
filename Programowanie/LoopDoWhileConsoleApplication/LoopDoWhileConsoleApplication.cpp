c9590fccb78f2c3b5ce3c0a0ef01947437ac2df3
MrPlax <aadam.blank@gmail.com>
2023-11-07 09:53:26 +0100

    hubeebhbebebebub

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -54,23 +54,63 @@ void task2()
 
 void task3()
 {
-	int randomNumber, numberFromUser;
+	const int LOWER_RANGE = 1;
+	const int UPPER_RANGE = 100;
+	srand(time(NULL));
+	int randomNumber = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+
+	//std::cout << randomNumber << "\n";
+
+	/*
+	if (numberFromUser != randomNumber)
+	std::cout << "Podaj liczbe";
+	std::cin >> numberFromUser
+		{
+		if (numberFromUser != randomNumber)
+		std::cout << "Podaj liczbe";
+		std::cin >> numberFromUser
+		}...
+	*/
+
+	int numberFromUser;
 	do
 	{
-		randomNumber = rand();
-		std::cout << "Podaj liczbe \n";
+		std::cout << "Podaj cyfre\n";
 		std::cin >> numberFromUser;
-		std::cout << randomNumber; 
-	} while (numberFromUser = randomNumber);
-		std::cout << "WYGRA£EŒ\n";
-
-	
-
-
-
+		if (numberFromUser > randomNumber)
+			std::cout << "mniej\n";
+		if (numberFromUser < randomNumber)
+			std::cout << "wiêcej\n";
+	} while (numberFromUser != randomNumber);
+	std::cout << "Gratulacje!!!!\n";
 }
 
+void task4()
+//napisz program wyœwietlaj¹cy l. calkowite z przedzia³u  <1,x>
+//gdzie x podaje u¿ytkownik
+{
+	//std::cout << "1,2,3,4,5,6";
+	unsigned long long upperRange, currentNumber;
+	std::cout << "Podaj górny zakres wiêkszy b¹dŸdŸ równy 1 \n";
+	std::cin >> upperRange;
+	/*std::cout << "1, ";
+	if (upperRange > 1)
+	{
+		std::cout << "2, ";
+		if (upperRange > 2)
+		{
+			std::cout << "3, ";
+			if (upperRange > 3)
+		}
+	}....*/
 
+	unsigned long long upperRange = 0;
+	do
+	{
+		currentNumber = currentNumber + 1;
+		std::cout << currentNumber << ", ";
+	} while (upperRange > currentNumber);
+}
 
 
 
@@ -78,5 +118,6 @@ int main()
 {
 	//task1();
 	//task2();
-	task3();
+	//task3();
+	task4();
 }
