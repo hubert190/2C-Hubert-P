bcc93fa245fe038e50c29b49fef7e4f57cffbc90
MrPlax <aadam.blank@gmail.com>
2023-11-14 09:43:56 +0100

    yjuhbgui

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -85,64 +85,146 @@ void task3()
 	std::cout << "Gratulacje!!!!\n";
 }
 
+//Napisz program wyœwietlaj¹cy liczby ca³kowite z przedzia³u <1,x>.
+//Gdzie x pdaje u¿ytkownika.
 void task4()
-//napisz program wyœwietlaj¹cy l. calkowite z przedzia³u  <1,x>
-//gdzie x podaje u¿ytkownik
 {
-	//std::cout << "1,2,3,4,5,6";
-	
-	unsigned long long upperRange, currentNumber;
-	std::cout << "Podaj górny zakres wiêkszy b¹dŸdŸ równy 1 \n";
+	//std::cout << "1, 2, 3, 4, 5, 6 \n";
+	unsigned long long upperRange;
+	std::cout << "Podaj górny zakres wiêkszy b¹dŸ równy 1\n";
 	std::cin >> upperRange;
-	
-	/*std::cout << "1, ";
+
+	/*
+	std::cout << "1, ";
 	if (upperRange > 1)
 	{
 		std::cout << "2, ";
 		if (upperRange > 2)
 		{
-
 			std::cout << "3, ";
 			if (upperRange > 3)
+			{
+				std::cout << "4, ";
+				//.....
+			}
 		}
-	}....*/
-
+	}
+	*/
 
 	unsigned long long currentNumber = 0;
 	do
 	{
-
-
-		//currentNumber= currentNumber+1
-		//currentNumber++
+		//currentNumber = currentNumber + 1;
 		//currentNumber += 1;
-		//++currentNumber
-		std::cout << currentNumber << " , ";
+		//currentNumber++;
+		++currentNumber;
+		std::cout << currentNumber << ", ";
 	} while (upperRange > currentNumber);
 }
 
+//Napisz program, który policzy sumê cyfr podanej przez u¿ytkownika liczby.
 void task5()
 {
 	int number;
-	std::cout << "Podaj liczbe ";
+	std::cout << "Podaj liczbê\n";
 	std::cin >> number;
-	
-	int sum = 1;
+
+	int sum = 0;
 	int rest;
 
+	/*
 	rest = number % 10;
-	sum = rest + sum;
+	sum = sum + rest;
 	number = number / 10;
+	if (number != 0)
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+		if (number != 0)
+		{
+			rest = number % 10;
+			sum = sum + rest;
+			number = number / 10;
+			if (number != 0)
+			{
+				rest = number % 10;
+				sum = sum + rest;
+				number = number / 10;
+				//if (number != 0) ...
+			}
+		}
+	}
+	*/
 
-	
+	do
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+	} while (number != 0);
 
+	std::cout << "Suma " << sum << "\n";
+	//4125
+}
 
+//Poproœ uzytkownika o liczbe do puki nie poda 0
+//Oblicz œredni¹ arytmetyczn¹ podanych liczb
+void task6()
+{
+
+	int numberFromUser, sum, rest,number;
+	sum = 0;
+	rest = 0;
+
+	/*if (numberFromUser != 0);
+	{
+		std::cout << "Podaj liczbê\n ";
+		std::cin >> numberFromUser;
+		sum += numberFromUser;
+		rest += 1;
+	}
+
+		if (numberFromUser != 0);
+		{
+			std::cout << "Podaj liczbê\n ";
+			std::cin >> numberFromUser;
+			sum += numberFromUser;
+			rest += 1;                                   
 		}
 
 
+			if (numberFromUser != 0);
+			{
+			std::cout << "Podaj liczbê\n ";
+			std::cin >> numberFromUser;
+			sum += numberFromUser;
+			rest += 1;
+			}
+				if... */
+
+
+	do
+	{
+		std::cout << "Podaj liczbê\n ";
+		std::cin >> numberFromUser;
+		sum += numberFromUser;
+		rest += 1;
+	} while (numberFromUser != 0);
+
+	number = sum / rest;
+
+	std::cout << "Srednia arytmetyczna wynosi " << number;
+
+
+	
 }
 
-
+void task7()
+{
+	
+	
+}
 
 
 int main()
@@ -151,5 +233,11 @@ int main()
 	//task2();
 	//task3();
 	//task4();
-	task5();
+	//task5();
+task6();
+	//task7();
 }
+
+
+
+
