aa17102b92fa11a297188f63d3613c26bb91156c
admin <admin@gmail.com>
2024-11-14 09:14:56 +0100

    GG

--- Programowanie/InheritanceConsoleApplication/SavingBankAccount.h
+++ Programowanie/InheritanceConsoleApplication/SavingBankAccount.h
@@ -1,1 +1,17 @@
 #include "BankAccount.h"
+
+class SavingBankAccount : public BankAccount
+{
+public:
+	double interestRate;
+	
+	public
+		SavingBankAccount()
+	{
+		interestRate = 0.1;
+
+		void Calcul
+			balance = balance + balance + interestRate; 
+		}
+	}
+};
