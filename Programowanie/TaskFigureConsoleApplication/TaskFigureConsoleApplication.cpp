92f83a3fd2e8302e4c06bfa81ce3652c84df7806
admin <admin@gmail.com>
2024-11-25 09:49:31 +0100

    gg

--- Programowanie/TaskFigureConsoleApplication/TaskFigureConsoleApplication.cpp
+++ Programowanie/TaskFigureConsoleApplication/TaskFigureConsoleApplication.cpp
@@ -1,35 +1,86 @@
 #include <iostream>
 #include <cmath>
 
-class Figure {
-public:
 
-
-};
-
-class Trapeze : public Figure {
+class Trapeze {
 private:
 	double a, b, h, c, d;
 public:
 	Trapeze(double a, double b, double h, double c, double d)
-		: a(a), b(b), h(h), c(c), d(d)
-}
+		: a(a), b(b), h(h), c(c), d(d) {}
 
-double CalculateTheArea()
-{
-	return
-		((a + b) * h) / 2;
-}
+	double area() {
+		return ((a + b) / 2) * h;
+	}
+	double circuit() {
+		return a + b + c + d;
+	}
+};
 
-double CalculateCircumference()
-{
-	return
-		(a + b + c + d);
-}
+class Rhombus {
+public:
+	Rhombus(double a, double h) : a(a), h(h) {}
+	double area() {
+		return a * h;
+	}
+	double circuit() {
+		return 4 * a;
+	}
+private:
+	double a, h;
+};
+
+class Parallelogram {
+private: 
+		double a, b, h;
+
+public:
+	Parallelogram(double a, double b, double h) : a(a),b(b), h(h) {}
+	double area() {
+		return a * h;
+	}
+
+	double circuit() {
+		return 2 * (a + b);
+	}
+};
+
+class Rectangle {
+private:
+	double a, b, h;
+
+public:
+	Rectangle(double a, double h) : a(a), h(h) {}
+	double area() {
+		return a * b;
+	}
+
+	double circuit() {
+		return 2 * (a + b);
+	}
+};
+
+class Square {
+private:
+	double a, b, h;
+
+public:
+	Square(double a) : a(a) {}
+	double area() {
+		return a * a;
+	}
+
+	double circuit() {
+		return 4*a;
+	}
+};
+
 
 int main()
 {
-  
+	Trapeze Trapeze(4,4,3, 4, 5);
+	std::cout << "pole trapezu: " << Trapeze.area() << "\n";
+	std::cout << "obwód trapezu: " << Trapeze.circuit() << "\n";
 }
 
 
