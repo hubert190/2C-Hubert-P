a080c6c1bf6b43e24ad5dd7beeb3be97f22824a0
Sala213 <Sala213@ezse.pl>
2025-09-10 15:57:52 +0200

    jhfkjhg

--- Programowanie/firstConsoleApp/Program.cs
+++ Programowanie/firstConsoleApp/Program.cs
@@ -1,6 +1,35 @@
-﻿Console.Write("Hello, World! \n");
+﻿using System.Runtime.InteropServices;
+
+Console.Write("Hello, World! \n");
 Console.WriteLine("Hello, World!");
 
 string name = "Jan";
 string lastname = "Kowalski";
 Console.WriteLine("Witaj " + name + " " + lastname + " " + "tutaj!!!");
+Console.Write("Witaj {0} {1} tutaj!!!", name, lastname);
+
+Console.WriteLine($"Witaj {name} {lastname} tutaj!!!");
+Console.WriteLine("predkosc to km//h");
+Console.WriteLine(@"predkosc to km/h");
+
+int firstNumber = 5;
+int secondNumber = firstNumber;
+secondNumber++;
+Console.WriteLine($"Pierwsza liczba: {firstNumber}");
+Console.WriteLine($"Pierwsza liczba: {secondNumber}");
+
+//----------------------------------------------------
+
+//przekazywanie wartości
+void ParametrTest_v1(int p)
+{
+    Console.WriteLine($"Parametr: {p}");
+    p++;
+    Console.WriteLine($"Parametr: {p}");
+}
+
+firstNumber = 15;
+Console.WriteLine($"przed {firstNumber}");
+ParametrTest_v1(firstNumber);
+Console.WriteLine($"po {firstNumber}");
+//Parametr_v1(99);
