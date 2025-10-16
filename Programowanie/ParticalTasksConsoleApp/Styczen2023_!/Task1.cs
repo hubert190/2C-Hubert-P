6b74b178989cd39fe9bfe58d0bb25e574b870879
Sala213 <Sala213@ezse.pl>
2025-10-16 16:51:33 +0200

    fgdgh

---
+++ Programowanie/ParticalTasksConsoleApp/Styczen2023_!/Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticalTasksConsoleApp.Styczen2023__
+{
+    internal class Task1
+    {
+        public void Task()
+        {
+            Console.WriteLine("Podaj liczbe");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbe");
+            int b = int.Parse(Console.ReadLine());
+
+        }
+    }
+
+}
