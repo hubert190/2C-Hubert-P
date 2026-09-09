1045445c084d7b9ab716febbe47a2f101bcb7f00
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-09 11:14:52 +0200

    fe

---
+++ Programowanie/EventVSBinding/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EventVSBinding
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Programowanie/EventVSBinding/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace EventVSBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            /*Slider slider = sender as Slider;
+            if (rotationLabel is not null && slider is not null)
+                Slider slider = sender as Slider; */
+            
+            if (rotationLabel is not null && sender is Slider slider)
+            {
+                rotationLabel.Rotation = slider.Value;
+            }
+        }
+    }
+}
