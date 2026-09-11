10c0face74c9727a89e4311b53a42819c8c4af84
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-11 11:36:45 +0200

    sfsagf

---
+++ Programowanie/SimpleCalculator/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(FirstNumberEntry.Text, out int firstNumber)
+                && int.TryParse(SecondNumberEntry.Text, out int secondNumber))
+            {
+                resultLabel.Text = $"Wynik dodawania to {firstNumber + secondNumber}";
+                resultLabel.BackgroundColor = Colors.Green;
+            }
+            else
+            {
+                resultLabel.Text = $"Podałeś nieprawidłowe dane";
+                resultLabel.BackgroundColor = Colors.Red;
+            }
+        }
+
+    }
+}
---
+++ Programowanie/SimpleCalculator1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleCalculator1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(FirstNumberEntry.Text, out int firstNumber)
+                && int.TryParse(SecondNumberEntry.Text, out int secondNumber))
+            {
+                resultLabel.Text = $"Wynik dodawania to {firstNumber + secondNumber}";
+                resultLabel.BackgroundColor = Colors.Green;
+            }
+            else
+            {
+                resultLabel.Text = $"Podałeś nieprawidłowe dane";
+                resultLabel.BackgroundColor = Colors.Red;
+            }
+        }
+
+        }
+}
